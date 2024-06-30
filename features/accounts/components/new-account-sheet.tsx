146990b313ec62b1c9518530cc24f6119040af09
Luke Volpe <luke.volpe@gmail.com>
2024-06-30 21:23:07 +0100

    Added create account functionality

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -6,9 +6,29 @@ import {
   SheetTitle,
 } from '@/components/ui/sheet';
 import { useNewAccount } from '../hooks/use-new-account';
+import AccountForm from './account-form';
+import { insertAccountSchema } from '@/db/schema';
+import { z } from 'zod';
+import useCreateAccount from '@/features/accounts/api/use-create-accounts';
+
+const formSchema = insertAccountSchema.pick({
+  name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
 
 const NewAccountSheet = () => {
   const { isOpen, onClose } = useNewAccount();
+
+  const mutation = useCreateAccount();
+
+  const onSubmit = (values: FormValues) => {
+    mutation.mutate(values, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
+  };
 
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
@@ -19,6 +39,13 @@ const NewAccountSheet = () => {
             Create a new account to track your spending.
           </SheetDescription>
         </SheetHeader>
+        <AccountForm
+          onSubmit={onSubmit}
+          disabled={mutation.isPending}
+          defaultValues={{
+            name: '',
+          }}
+        />
       </SheetContent>
     </Sheet>
   );
