183c40547477d184d6e502a32eec300d435ecea2
Luke Volpe <luke.volpe@gmail.com>
2024-07-18 18:17:40 +0100

    Added date range filter as part of the header

--- components/account-filter.tsx
+++ components/account-filter.tsx
@@ -3,6 +3,7 @@
 import qs from 'query-string';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
+import { useGetSummary } from '@/features/summary/api/use-get-summary';
 import { useGetAccounts } from '@/features/accounts/api/use-get-accounts';
 
 import {
@@ -21,6 +22,8 @@ export const AccountFilter = () => {
   const accountId = params.get('accountId') || 'all';
   const from = params.get('from') || '';
   const to = params.get('to') || '';
+
+  const { isLoading: isLoadingSummary } = useGetSummary();
 
   const { data: accounts, isLoading: isLoadingAccounts } = useGetAccounts();
 
@@ -49,7 +52,7 @@ export const AccountFilter = () => {
     <Select
       value={accountId}
       onValueChange={onChange}
-      disabled={isLoadingAccounts}
+      disabled={isLoadingAccounts || isLoadingSummary}
     >
       <SelectTrigger className="lg:w-auto w-full h-9 rounded-md px-3 font-normal bg-white/10 hover:bg-white/20 hover:text-white border-none focus:ring-offset-0 focus:ring-transparent outline-none text-white focus:bg-white/30 transition">
         <SelectValue placeholder="Select account" />
